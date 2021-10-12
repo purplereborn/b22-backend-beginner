fef72e230a7ca6c43f5a2f0d4ee3502636bdef62
SANDI MUHAMAD RIZALUL <79769140+PurpleReborn@users.noreply.github.com>
2021-10-12 22:42:04 +0700

    fix cors 4

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ const socket = require('./src/middlewares/socket')
 //   }
 // })
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: 'https://sandi-coffeeshop-react.netlify.app'
-  }
-})
-
 // const io = require('socket.io')(server, {
 //   cors: {
-//     origin: 'http://localhost:8081'
+//     origin: 'https://sandi-coffeeshop-react.netlify.app'
 //   }
 // })
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+})
 
 app.use(socket(io))
 
